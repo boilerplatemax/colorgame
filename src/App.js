4726b0ec55b05fb5dbf42a2f2c73e85e5a45fbca
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-10-13 23:14:22 -0500

    optimized by removing for loops in place of mapping functions

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import React,{useState, useEffect} from 'react'
 import './App.css';
-const chars =['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-
-function getRandomColor(){
-  let randomColorArray =[]
-  for(var i=0;i<3;i++){
-    let color='#'
-      for(var j=0;j<6;j++){
-        color+=chars[Math.floor(Math.random()*chars.length)]
-      }
-      randomColorArray.push(color) 
-    }
-    return randomColorArray
-  }
 
 
 function App() {
-  const [randomNum, setRandomNum]=useState(Math.floor(Math.random()*3))
+  const [randomNum, setRandomNum]=useState('')
 
-  const [colorArray, setColorArray]=useState(getRandomColor())
+  const [colorArray, setColorArray]=useState([])
 
   const [answer, setAnswer]=useState('Guess the color!')
 
+  useEffect(()=>{
+    generateColors()
+  },[])
+  const chars =['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+  const getRandomColor=()=>{
+    const color = new Array(6).fill("").map(()=>chars[Math.floor(Math.random()*chars.length)]).join('')
+    return `#${color}`
+    
+  }
+  const generateColors=()=>{
+    setColorArray([getRandomColor(),getRandomColor(),getRandomColor()])
+    setRandomNum(Math.floor(Math.random()*3))
+  }
   const checkAnswerHandler=e=>{
     const correctColor=colorArray[randomNum]
     const guess =e.target.innerHTML
 
     if(guess===correctColor){
       setAnswer('Correct')
-      setColorArray(getRandomColor())
-      setRandomNum(Math.floor(Math.random()*3))
+      generateColors()
     }
     else{
       setAnswer('Wrong')
     }
   }
-
   return (
     <div className="App" style={{width:'100vw', height:'100vh', backgroundColor:colorArray[randomNum], position:'absolute'}}>
       <div className='title'>
@@ -44,11 +42,11 @@ function App() {
       </h1>
       </div>
       <div className='btn__container'>
-        <button onClick={checkAnswerHandler} className='btn'>{colorArray[0]}</button>
-        <button onClick={checkAnswerHandler} className='btn'>{colorArray[1]}</button>
-        <button onClick={checkAnswerHandler} className='btn'>{colorArray[2]}</button>
+        {colorArray.map((x, index)=>{
+          return(<button onClick={checkAnswerHandler} className='btn'>{colorArray[index]}</button>)
+        })}
+
       </div>
-      
     </div>
   );
 }
