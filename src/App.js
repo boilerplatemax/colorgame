994bfd51388dff175786d2e7b741a457f45aaf53
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-10-14 13:25:59 -0500

    optimized code by figuring out useEffect issues, removed random number generator to select a color in place of a shuffle function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import './App.css';
 
 const chars =['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
@@ -8,39 +8,53 @@ const getRandomColor=()=>{
   
 }
 function App() {
-  const [randomNum, setRandomNum]=useState(Math.floor(Math.random()*3))
+  const [color,setColor]=useState('')
 
-  const [colorArray, setColorArray]=useState([getRandomColor(),getRandomColor(),getRandomColor()])
+  const [colorArray, setColorArray]=useState([])
+  //this bool state could have been omitted however it helps with readability and it makes sense to check a bool rather than checking if a string === true/false
+  const [answer, setAnswer]=useState(true)
+  const [message, setMessage]=useState('Guess the color!')
+  //passing answer as a dependency will run this code on page load but also whenever the answer changes
+  useEffect(()=>{
 
-  const [answer, setAnswer]=useState('Guess the color!')
+    const generateColors=()=>{
+      const actualColor=getRandomColor()
+      setColor(actualColor)
+      //0.5-Math.random() will either return a negative or positive number, depending on result each array element will either stay in its spot or shift which creates a simple shuffle mechanic
+      setColorArray([actualColor,getRandomColor(),getRandomColor()].sort(()=>0.5-Math.random()))
+    
+    }
+    //only regenerate colours when the user is correct
+  if(answer)generateColors()
+  },[answer])
 
-
-  const generateColors=()=>{
-    setColorArray([getRandomColor(),getRandomColor(),getRandomColor()])
-    setRandomNum(Math.floor(Math.random()*3))
-  }
   const checkAnswerHandler=e=>{
-    const correctColor=colorArray[randomNum]
     const guess =e.target.innerHTML
-
-    if(guess===correctColor){
-      setAnswer('Correct')
-      generateColors()
+    if(guess===color){
+      setAnswer(true)
+      setMessage('Correct')
     }
     else{
-      setAnswer('Wrong')
+      setAnswer(false)
+      setMessage('Wrong')
     }
   }
   return (
-    <div className="App" style={{width:'100vw', height:'100vh', backgroundColor:colorArray[randomNum], position:'absolute'}}>
+    <div className="App" style={{width:'100vw', height:'100vh', backgroundColor:color, position:'absolute'}}>
       <div className='title'>
-        <h1 className={answer==='Correct'?'correct':'false'}>
-        {answer}
+        <h1 className={answer?'correct':'false'}>
+        {message}
       </h1>
       </div>
       <div className='btn__container'>
         {colorArray.map((x, index)=>{
-          return(<button onClick={checkAnswerHandler} key={index} className='btn'>{colorArray[index]}</button>)
+          return(
+          <button
+          onClick={checkAnswerHandler}
+          key={index} className='btn'>
+            {colorArray[index]}
+          </button>
+          )
         })}
 
       </div>
